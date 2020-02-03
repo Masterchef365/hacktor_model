4899551e70d3b939dbef00438448b3b8cb947277
Masterchef365 <duncan.freeman1@gmail.com>
2020-02-03 13:50:19 -0800

    Add render system deps

---
+++ src/render_system.rs
@@ -0,0 +1,13 @@
+use crate::anonymous::*;
+use crate::common_types::{Message, System, SystemID};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+pub struct RenderSystem {
+}
+
+impl RenderSystem {
+    pub fn new() -> Self {
+        RenderSystem
+    }
+}
